aeb0704a8743c955f5ad5f54ee619236e0a61519
Faming Hou <faming.hou@gmail.com>
2019-08-07 21:21:23 +1200

    Feature/cm 1 nlog (#1)
    * configure logging by using NLog
    
    * change configuration
    
    * document of NLog

---
+++ CustomerManagement.Base/Configuration/LogConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+using NLog.Web;
+using System;
+using System.IO;
+
+namespace CustomerManagement.Base.Configuration
+{
+    public static class LogConfiguration
+    {
+        public static Logger Configure()
+        {
+            var basePath = AppDomain.CurrentDomain.RelativeSearchPath ?? AppDomain.CurrentDomain.BaseDirectory;
+            var filePath = $"{basePath}/nlog.config";
+            var aspnetEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var environmentSpecificFilePath = $"{basePath}/nlog.{aspnetEnvironment}.config";
+            if (File.Exists(environmentSpecificFilePath))
+                filePath = environmentSpecificFilePath;
+            return NLogBuilder.ConfigureNLog(filePath).GetCurrentClassLogger();
+        }
+    }
+}
